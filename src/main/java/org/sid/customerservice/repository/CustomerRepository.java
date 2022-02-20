52d69f3ea8d917e6601e8c8e9afe6d5edc5c0c1a
elayoubidriss <drisselayoubi4@gmail.com>
2022-02-20 20:21:46 +0100

    Initial commit

---
+++ src/main/java/org/sid/customerservice/repository/CustomerRepository.java
@@ -0,0 +1,11 @@
+package org.sid.customerservice.repository;
+
+import org.sid.customerservice.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@RepositoryRestResource
+@CrossOrigin(origins = "http://localhost:4200")
+public interface CustomerRepository extends JpaRepository<Customer,Long> {
+}
